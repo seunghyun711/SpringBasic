cd970ad20933d9ad2af53c37df662a6f17e31392
seunghyun711 <seunghyun711@gmail.com>
2023-01-23 23:42:46 +0900

    데이터 삭제

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -113,5 +114,25 @@ public class ArticleController {
 
         // 3. 수정 결과 페이지로 리다이렉트 한다.
         return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    // 데이터 삭제
+    // html에서 post,get외에 다른 메서드 제공 안함 따라서 삭제 시 임의호 get방식 사용
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){ // RedirectAttribute : 삭제된 경우 해당 클래스의 addFlashAttributes()메서드를 총해 일회성으로 사용할 휘발성 데이터를 등록한다.
+        log.info("삭제 요청이 들어왔습니다.");
+
+        // 1. 삭제 대상을 가져온디.
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        // 2. 대상 삭제
+        if(target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제가 완료되었습니다.");
+        }
+
+        // 3. 결과 페이지로 리다이렉트한다.
+        return "redirect:/articles";
     }
 }
