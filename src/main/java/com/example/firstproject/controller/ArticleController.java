1260a285262886461df76985874fe369dd1aab6a
seunghyun711 <seunghyun711@gmail.com>
2023-01-28 14:08:23 +0900

    댓글 목록 뷰 만들기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,6 +22,9 @@ public class ArticleController {
 
     @Autowired // 스프링 부트가 미리 생성한 객체를 가져다가 자동으로 연결
     private ArticleRepository articleRepository;
+
+    @Autowired
+    private CommentService commentService;
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
@@ -60,9 +65,11 @@ public class ArticleController {
          */
         // 1. id로 데이터 가져옴
         Article articleEntity = articleRepository.findById(id).orElse(null); // id값을 통해 찾았는데 해당 id값이 없다면 null리턴
+        List<CommentDto> commentDtos = commentService.comments(id);
 
         // 2. 가져온 데이터를 Model에 등록
         model.addAttribute("article",articleEntity);
+        model.addAttribute("commentDtos",commentDtos);
 
         // 3. 보여줄 페이지 설정
         return "articles/show";
