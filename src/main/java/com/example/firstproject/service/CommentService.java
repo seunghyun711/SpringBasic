5e0aafd2a34c38d1f19d9899c64cc4b61d8d62cd
seunghyun711 <seunghyun711@gmail.com>
2023-01-28 12:13:32 +0900

    CommentService수정(for문->stream사용)

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -22,14 +23,17 @@ public class CommentService {
         List<Comment> comments = commentRepository.findByArticleId(articleId);
 
         // 변환 : 엔티티 -> dto형태
-        List<CommentDto> dtos = new ArrayList<>();
-        for (int i = 0; i < comments.size(); i++) {
-            Comment c = comments.get(i);
-            CommentDto dto = CommentDto.createCommentDto(c); // 클래스 메서드 호출
-            dtos.add(dto);
-        }
+//        List<CommentDto> dtos = new ArrayList<>();
+//        for (int i = 0; i < comments.size(); i++) {
+//            Comment c = comments.get(i);
+//            CommentDto dto = CommentDto.createCommentDto(c); // 클래스 메서드 호출
+//            dtos.add(dto);
+//        }
 
-        // 반환
-        return dtos;
+        // 반환(변환 과정에서 for문을 스트림을 사용한 방식으로 변경
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
     }
 }
