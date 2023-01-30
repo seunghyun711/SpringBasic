7d073c535e08a8e56b247d07a4eb318a5edf304e
seunghyun711 <seunghyun711@gmail.com>
2023-01-30 23:26:34 +0900

    No Ioc & DI / Test

---
+++ src/main/java/com/example/firstproject/ioc/Beef.java
@@ -0,0 +1,13 @@
+package com.example.firstproject.ioc;
+
+public class Beef {
+    private String name;
+
+    public Beef(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/com/example/firstproject/ioc/Chef.java
+++ src/main/java/com/example/firstproject/ioc/Chef.java
@@ -4,9 +4,11 @@ public class Chef {
 
     public String cook(String menu) {
         // 재료 준비
-        Pork pork = new Pork("한돈 등심");
+        // 아래 방식은 pork에 대한 내용이 필요하면 Pork 인스턴스를 생성해야하고, Beef에 대한 내용이 필요하면 Beef인스턴스를 직접 작성해야 한다.
+//        Pork pork = new Pork("한돈 등심");
+        Beef beef = new Beef("한우 꽃등심");
 
         // 요리 반환
-        return pork.getName() + "으로 만든 " + menu;
+        return beef.getName() + "으로 만든 " + menu;
     }
 }
