86a08c3e1ef97a0fd382458bad12528962e22c1c
seunghyun711 <seunghyun711@gmail.com>
2023-02-01 20:41:35 +0900

    객체를 JSON으로 전환

---
+++ src/main/java/com/example/firstproject/objectmapper/Burger.java
@@ -0,0 +1,16 @@
+package com.example.firstproject.objectmapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@ToString
+@Getter
+public class Burger {
+    private String name;
+    private int price;
+    private List<String> ingredients;
+}
---
+++ src/test/java/com/example/firstproject/objectmapper/BurgerTest.java
@@ -0,0 +1,31 @@
+package com.example.firstproject.objectmapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BurgerTest {
+    @Test
+    public void 자바_객체를JSON으로_변환() throws JsonProcessingException {
+        // 준비
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<String> ingredients = Arrays.asList("치즈","패티","양파");
+        Burger burger = new Burger("콰트로 치즈 와퍼", 7000, ingredients);
+
+        // 수행
+        String json = objectMapper.writeValueAsString(burger);
+
+        // 예상
+        String expected = "{\"name\":\"콰트로 치즈 와퍼\",\"price\":7000,\"ingredients\":[\"치즈\",\"패티\",\"양파\"]}";
+
+        // 검증
+        assertEquals(expected, json);
+        System.out.println(json);
+    }
+
+}
