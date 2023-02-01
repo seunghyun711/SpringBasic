7e5457f773d6e6d7aa8eae7efeb5ba22439e5c71
seunghyun711 <seunghyun711@gmail.com>
2023-02-01 20:53:47 +0900

    Json을 객체로 전환

--- src/main/java/com/example/firstproject/objectmapper/Burger.java
+++ src/main/java/com/example/firstproject/objectmapper/Burger.java
@@ -2,6 +2,7 @@ package com.example.firstproject.objectmapper;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @AllArgsConstructor
 @ToString
 @Getter
+@NoArgsConstructor
 public class Burger {
     private String name;
     private int price;
--- src/test/java/com/example/firstproject/objectmapper/BurgerTest.java
+++ src/test/java/com/example/firstproject/objectmapper/BurgerTest.java
@@ -31,4 +31,25 @@ class BurgerTest {
         System.out.println(jsonNode.toPrettyString());
     }
 
+    @Test
+    public void JSON을_자바_객체로_변환() throws JsonProcessingException{
+        // 준비
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = "{\"name\":\"콰트로 치즈 와퍼\",\"price\":7000,\"ingredients\":[\"치즈\",\"패티\",\"양파\"]}";
+
+        // 수행
+        // readValue를 통해 burger를 만들때 디폴트 생성자가 호출되므로 Burger클래스에 @NoArgsContructor를 추가해 디폴트 생성자를 추가한다.
+        Burger burger = objectMapper.readValue(json, Burger.class);
+
+        // 예상
+        List<String> ingredients = Arrays.asList("치즈","패티","양파");
+        Burger expected = new Burger("콰트로 치즈 와퍼", 7000, ingredients);
+
+        // 검증
+        assertEquals(expected.toString(), burger.toString());
+        JsonNode jsonNode = objectMapper.readTree(json);
+        System.out.println(jsonNode.toPrettyString());
+        System.out.println(burger.toString());
+    }
+
 }
